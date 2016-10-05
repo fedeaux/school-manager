a9393ea67a65bd13734027b51cd509ec3fd4fddf
Pedro Bernardes <phec06@gmail.com>
2016-10-05 10:19:37 -0300

    Student model with validations, factories and enumerate status

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,6 @@
 class Student < ApplicationRecord
+  validates :name, presence: true
+  validates :register_number, presence: true
+  validates :status, presence: true
+  has_enumeration_for :status, create_helpers: true, create_scopes: true
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Student, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'factories' do
+    it 'has a valid active factory' do
+      active_student = create :student
+      expect(active_student).to be_valid
+      expect(active_student.active?).to eq true
+    end
+
+    it 'has a valid inactive factory trait' do
+      active_student = create :student, :inactive
+      expect(active_student).to be_valid
+      expect(active_student.inactive?).to eq true
+    end
+  end
+
+  describe 'validations' do
+    it 'is invalid without a name' do
+      expect(Student.new attributes_for(:student).except(:name)).to be_invalid
+    end
+
+    it 'is invalid without a register_number' do
+      expect(Student.new attributes_for(:student).except(:register_number)).to be_invalid
+    end
+
+    it 'is invalid without a status' do
+      expect(Student.new attributes_for(:student).except(:status)).to be_invalid
+    end
+  end
 end
